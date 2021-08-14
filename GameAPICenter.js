17bc0f293828a0f1ed8334eb8b123aeda67adcd0
MattPlays <markgpmcpe@gmail.com>
2021-08-14 17:03:52 -0400

    Clean Code and Seperate Packages

--- GameAPICenter.js
+++ GameAPICenter.js
@@ -1,24 +1,14 @@
-const ApexLegendsAPI = require("./APIs/ApexLegends/ApexLegendsAPI").ApexLegendsAPI;
-const OverwatchAPI = require("./APIs/Overwatch/OverwatchAPI").OverwatchAPI;
-const PUBGAPI = require("./APIs/PUBG/PUBGAPI").PUBGAPI;
-const ClashOfClansAPI = require("./APIs/Supercell/Clash of Clans/ClashOfClansAPI").ClashOfClansAPI;
-const BrawlStarsAPI = require("./APIs/Supercell/Brawl Stars/BrawlStarsAPI").BrawlStarsAPI;
-const ValorantAPI = require("./APIs/Valorant/ValorantAPI").ValorantAPI;
-const BloonsTD6API = require("./APIs/Bloons TD 6/BloonsTD6API").BloonsTD6API;
-const ClashRoyalAPI = require("./APIs/Supercell/Clash Royal/ClashRoyalAPI").ClashRoyalAPI;
-const CSGOAPI = require("./APIs/Counter Strike Global Offensive/CSGOAPI").CSGOAPI;
-const HyperScapeAPI = require("./APIs/Hyper Scape/HyperScapeAPI").HyperScapeAPI;
-const MinecraftAPI = require("./APIs/Minecraft/MinecraftAPI").MinecraftAPI;
+const ApexLegendsAPI = require("@mattplays/apexlegends-api").ApexLegendsAPI;
+const BloonsTD6API = require("@mattplays/bloonstd6-api").BloonsTD6API;
+const CSGOAPI = require("@mattplays/csgo-api").CSGOAPI;
+const HyperScapeAPI = require("@mattplays/hyperscape-api").HyperScapeAPI;
+const MinecraftAPI = require("@mattplays/minecraft-api").MinecraftAPI;
+const OverwatchAPI = require("@mattplays/overwatch-api").OverwatchAPI;
 module.exports = {
     ApexLegendsAPI: ApexLegendsAPI,
-    OverwatchAPI: OverwatchAPI,
-    PUBGAPI: PUBGAPI,
-    ClashOfClansAPI: ClashOfClansAPI,
-    BrawlStarsAPI: BrawlStarsAPI,
-    ValorantAPI: ValorantAPI,
     BloonsTD6API: BloonsTD6API,
-    ClashRoyalAPI: ClashRoyalAPI,
     CSGOAPI: CSGOAPI,
     HyperScapeAPI: HyperScapeAPI,
-    MinecraftAPI: MinecraftAPI
-}
+    MinecraftAPI: MinecraftAPI,
+    OverwatchAPI: OverwatchAPI,
+};
