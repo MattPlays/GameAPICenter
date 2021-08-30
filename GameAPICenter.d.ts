758e67d692ae5b8f522a3825bca1e95dcf2b6f24
MattPlays <markgpmcpe@gmail.com>
2021-08-30 10:27:38 -0400

    Fix Code Base

--- GameAPICenter.d.ts
+++ GameAPICenter.d.ts
@@ -1,13 +1,8 @@
-import { ApexLegendsAPI } from "./APIs/ApexLegends/ApexLegendsAPI";
-import { OverwatchAPI } from "./APIs/Overwatch/OverwatchAPI";
-import { PUBGAPI } from "./APIs/PUBG/PUBGAPI";
-import { ClashOfClansAPI } from "./APIs/Supercell/Clash of Clans/ClashOfClansAPI";
-import { BrawlStarsAPI } from "./APIs/Supercell/Brawl Stars/BrawlStarsAPI";
-import { ValorantAPI } from "./APIs/Valorant/ValorantAPI";
-import { BloonsTD6API } from "./APIs/Bloons TD 6/BloonsTD6API";
-import { ClashRoyalAPI } from "./APIs/Supercell/Clash Royal/ClashRoyalAPI";
-import { CSGOAPI } from "./APIs/Counter Strike Global Offensive/CSGOAPI";
-import { HyperScapeAPI } from "./APIs/Hyper Scape/HyperScapeAPI";
-import { MinecraftAPI } from "./APIs/Minecraft/MinecraftAPI";
+import {ApexLegendsAPI} from "@mattplays/apexlegends-api";
+import {BloonsTD6API} from  "@mattplays/bloonstd6-api";
+import {CSGOAPI} from  "@mattplays/csgo-api";
+import {HyperScapeAPI} from "@mattplays/hyperscape-api";
+import {MinecraftAPI} from "@mattplays/minecraft-api";
+import {OverwatchAPI} from "@mattplays/overwatch-api";
 
-export {ApexLegendsAPI, OverwatchAPI, PUBGAPI, ClashOfClansAPI, BrawlStarsAPI, ValorantAPI, BloonsTD6API, ClashRoyalAPI, CSGOAPI, HyperScapeAPI, MinecraftAPI}
+export {ApexLegendsAPI, OverwatchAPI, BloonsTD6API, CSGOAPI, HyperScapeAPI, MinecraftAPI}
